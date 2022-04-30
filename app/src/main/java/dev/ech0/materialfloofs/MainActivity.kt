4146676eb62578830ba38df0ef648d4f20b9171a
winterfoxxy <winterarctic26@outlook.com>
2022-04-30 15:38:42 -0400

    initial

---
+++ app/src/main/java/dev/ech0/materialfloofs/MainActivity.kt
@@ -0,0 +1,17 @@
+package dev.ech0.materialfloofs
+
+import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.appbar.MaterialToolbar
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
